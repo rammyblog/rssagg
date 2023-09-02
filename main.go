d3577650a656ba3137ed6c508326744b46890ae4
Babatunde Onasanya <onasanyatunde67@gmail.com>
2023-09-02 22:48:14 +0100

    boilerplate for web

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port is not available")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+	v1Router := chi.NewRouter()
+
+
+	v1Router.Get("/health", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+
+
+	router.Mount("/v1", v1Router)
+
+
+	log.Printf("Server starting on port %v", port)
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
